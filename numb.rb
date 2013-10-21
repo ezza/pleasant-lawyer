dbc2ba327662f95219bcf8adf876baf64bd2cb7a
Erin Francis <ezzzaf@gmail.com>
2013-10-22 12:02:13 +1300

    Add words to number method and remove duplicate word

--- numb.rb
+++ numb.rb
@@ -10,6 +10,13 @@ def number_to_words(n)
   [ADJECTIVES[numbers[0]], NOUNS[numbers[1]]]
 end
 
+def words_to_number(adjective, noun)
+  adjective_index = ADJECTIVES.index{|word| word[0..2] == adjective[0..2]}
+  noun_index = NOUNS.index{|word| word[0..2] == noun[0..2]}
+  ((noun_index - adjective_index) % WORD_LIST_SIZE ) * WORD_LIST_SIZE + adjective_index + 1
+end
+
 (1..WORD_LIST_SIZE**2).each do |i|
-  puts number_to_words(i).join(' ')
+  puts "#{i} | " + number_to_words(i).join(' ') + " | " + words_to_number(*number_to_words(i)).to_s
+  raise 'fail!' unless i == words_to_number(*number_to_words(i))
 end
